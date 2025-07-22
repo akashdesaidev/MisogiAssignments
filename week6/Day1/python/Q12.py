64c901838d1121ab1933b4692d3ef70e827698fd
Akash Desai <132215036+AakashDesai3105@users.noreply.github.com>
2025-07-22 17:23:41 +0530

    Add a new Python script for user input and greeting. The script prompts for the user's name, age, city, and hobby, then prints a personalized message.

---
+++ week6/Day1/python/Q12.py
@@ -0,0 +1,14 @@
+name=input("Enter your full name: ")
+age=int(input("Enter your age: "))
+city=input("Enter your city: ")
+hobby=input("Enter your hobby: ")
+print(f"""Hello {name}!
+You are {age} years old and live in {city}.
+In your free time, you enjoy {hobby}.""")
+
+
+
+
+
+
+
