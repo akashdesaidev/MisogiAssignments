39c59fa9dde985c2c58a45ae4ea1c4413e46995b
Akash Desai <132215036+AakashDesai3105@users.noreply.github.com>
2025-07-23 11:02:36 +0530

    Q18 completed

---
+++ week6/Day1/python/Q18/one_liner_challenges.py
@@ -0,0 +1,11 @@
+
+from functools import reduce
+
+evenSquare=[i*i for i in range(1,11) if i%2==0]
+print(evenSquare)
+
+Captilalize = list(map(str.capitalize, ['hello', 'world',"check"]))
+print(Captilalize)
+
+result  = reduce(lambda a,b: a+b ,evenSquare)
+print(result)
