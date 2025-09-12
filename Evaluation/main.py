6c1daab7540a35bbd0405cf5fd1cfecc29e09a24
Akash Desai <132215036+AakashDesai3105@users.noreply.github.com>
2025-09-12 15:27:21 +0530

    Refactor main.py to use environment variable for port configuration, enhancing flexibility for deployment

--- Evaluation/main.py
+++ Evaluation/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI,UploadFile, File
 from router.ingestRouter import ingestRouter
 from router.queryRouter import queryRouter
-
+import os
 app = FastAPI()
 
 @app.get("/")
@@ -13,4 +13,5 @@ app.include_router(ingestRouter)
 app.include_router(queryRouter)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000,reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port,reload=True)
